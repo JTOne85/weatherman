4bd82416cb9f33264e4a98c3404188855e02c03a
Jacques Terblanche <jacques.terblanche@dotdigital.com>
2020-10-26 19:01:21 +0200

    Adding new service for api connection

--- Weatherman.Console/Weatherman.Console/Program.cs
+++ Weatherman.Console/Weatherman.Console/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Configuration;
+using Weatherman.Core.Services;
 
 namespace Weatherman.Console
 {
@@ -6,24 +7,24 @@ namespace Weatherman.Console
     {
         public static void Main(string[] args)
         {
-            int inputValue = 0;
-            var shouldContinue = DisplayMenu(out inputValue);
+            var shouldContinue = DisplayMenu();
             if (shouldContinue)
             {
                 DisplayWeatherMenu();
             }
         }
 
-        private static bool DisplayMenu(out int inputValue)
+        private static bool DisplayMenu()
         {
-            System.Console.WriteLine(
-                "Welcome to the Weather Service console app. Please select an option from the menu:");
+            System.Console.WriteLine("Welcome to the Weather Service console app. Please select an option from the menu:");
             System.Console.WriteLine("(1.) Choose a city");
             System.Console.WriteLine("(2.) Exit");
+            int inputValue;
             var success = int.TryParse(System.Console.ReadLine(), out inputValue);
             if (!success)
             {
                 System.Console.WriteLine("Please use numbers as input");
+                DisplayMenu();
             }
 
             return success;
@@ -31,27 +32,40 @@ namespace Weatherman.Console
 
         private static void DisplayWeatherMenu()
         {
-            var cityInput = 0;
             System.Console.WriteLine("We currently only support Cape Town's weather");
             System.Console.WriteLine("(1.) Cape Town");
             System.Console.WriteLine("(2.) Exit");
-            if (!int.TryParse(System.Console.ReadLine(), out cityInput))
+            var input = System.Console.ReadLine();
+            int cityInput;
+            var success = int.TryParse(input, out cityInput);
+            if (!success)
             {
                 System.Console.WriteLine("Please use the correct key to select data. 1 or 2");
                 DisplayWeatherMenu();
             }
-
-            else if (cityInput == 2)
-            {
-                System.Console.WriteLine("Have a good day!");
-            }
             else
             {
-                System.Console.WriteLine("Calling the weatherman...");
-                // call the weather api here
+                switch (cityInput)
+                {
+                    case 1:
+                        System.Console.WriteLine("Calling the weatherman...");
+                        RetrieveDataFromOpenWeather("Cape Town");
+                        break;
+                    case 2:
+                        System.Console.WriteLine("Have a good day!");
+                        break;
+                    default:
+                        break;
+                }
             }
+        }
+
+        private static void RetrieveDataFromOpenWeather(string location)
+        {
+            var apiKey = ConfigurationManager.AppSettings.Get("apiKey");
+            var result = WeatherService.GetWeatherByStringLocation(location, apiKey);
             
         }
-}
+    }
 
 }
---
+++ Weatherman.Console/Weatherman.Core/Services/WeatherService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net.Http;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace Weatherman.Core.Services
+{
+    public class WeatherService
+    {
+        public static async Task<string> GetWeatherByStringLocation(string location, string apiKey)
+        {
+            var encodedLocation = location.Replace(" ", "%20");
+            var baseUrl = $"http://api.openweathermap.org/data/2.5/weather?q={encodedLocation}&appid={apiKey}";
+            var httpClient = new HttpClient();
+            var blah =  httpClient.GetAsync(baseUrl).Result;
+            blah.EnsureSuccessStatusCode();
+            string responseBody = await blah.Content.ReadAsStringAsync();
+            return responseBody;
+        }
+
+    }
+}
